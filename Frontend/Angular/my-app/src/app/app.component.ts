9a490df8682394499243bd11101e80b7d86599dd
trungtinh <huynhtrungtinh1996@gmail.com>
2018-08-24 17:06:20 +0700

    update all

--- Frontend/Angular/my-app/src/app/app.component.ts
+++ Frontend/Angular/my-app/src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
-
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  users : any;
+  apiURL = 'https://5b7f7cd4af5e5600144d5e8d.mockapi.io/TT';
+  constructor(private http: Http)
+  {
+    this.http.get(this.apiURL)
+    .toPromise()
+    .then(res => res.json())
+    .then(r => (this.users = r)
+  )};
+  sers= [
+    {
+        "id": "1",
+        "name": "Woodrow_Windler43@gmail.com"
+    },
+    {
+        "id": "2",
+        "name": "Yasmeen_King27@hotmail.com"
+    }
+  ]
+  id="";
+  name="";
+  EditPerson(id)
+  {
+    this.id = this.users[id].id;
+    this.name = this.users[id].id;
+  }
   title = 'my-app';
 }
+
